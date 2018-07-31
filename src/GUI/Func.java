2035aadef9c1cbd4b15ff1e39a42ee91616492d6
HDD <HDD@DESKTOP-TSG4VQP>
2018-07-31 13:27:06 +0900

    add com select func

--- src/GUI/Func.java
+++ src/GUI/Func.java
@@ -1,5 +1,7 @@
 package GUI;
 
+import java.util.HashMap;
+
 import jssc.SerialPort;
 import jssc.SerialPortException;
 import jssc.SerialPortList;
@@ -7,33 +9,60 @@ import jssc.SerialPortList;
 class Func {
 	
 	SerialPort MySerialPort;
+	String[] portNames;
+	String portName;
+	HashMap<String,Boolean> status = new HashMap<String,Boolean>();
 	
-	public Func() {
-		System.out.println("setup");
-		MySerialPort = new SerialPort("COM3");
+	public Func(String portName) {
+		portNames = SerialPortList.getPortNames();
+		for(int i=0; i<portNames.length; i++) {
+			status.put(portNames[i], false);
+		}
+		setPort(portName);
+		System.out.println("setuped"+portName);
+	}
+	
+	public void setPort(String portName) {
+		MySerialPort = new SerialPort(portName);
+		this.portName = portName;
+	}
+	
+	private void SendMessage(String message) {
 		try {
+		MySerialPort.writeString(message);
+		}catch(SerialPortException ex) {
+		System.out.println(ex);
+		}
+	}
+	
+	public void Connect() {
+		try {
+			System.out.println("Connect : "+portName);
 			MySerialPort.openPort();
 			MySerialPort.setParams(SerialPort.BAUDRATE_9600,SerialPort.DATABITS_8,SerialPort.STOPBITS_1,SerialPort.PARITY_NONE);
+			status.put(portName,true);
 		}catch(SerialPortException ex) {
 			System.out.println(ex);
-			}
+		}
+	}
+	
+	public void Disconnect() {
+		try {
+			System.out.println("Disconnect"+portName);
+			MySerialPort.closePort();
+			status.put(portName,false);
+		}catch(SerialPortException ex) {
+			System.out.println(ex);
+		}
 	}
 	
 	public void Cold() {
-		System.out.println("Cold");
-		try {
-			MySerialPort.writeString("(AN)");
-		}catch(SerialPortException ex) {
-			System.out.println(ex);
-		}
+		System.out.println("Cold"+portName);
+		SendMessage("(AN)");
 	}
 	
 	public void Off() {
-		System.out.println("Off");
-		try {
-			MySerialPort.writeString("(AO)");
-		}catch(SerialPortException ex) {
-			System.out.println(ex);
-		}
+		System.out.println("Off"+portName);
+		SendMessage("(AO)");
 	}
 }
--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -2,19 +2,55 @@ package GUI;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 class Test extends JFrame{
-	Func func = new Func();
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	JPanel pa = new JPanel();
 	JButton ON = new JButton("냉방");
 	JButton OFF = new JButton("끄기");
+	JButton Connect = new JButton("연결");
+	JButton Disconnect = new JButton("끊기");
+	JLabel title = new JLabel("에어컨 제어 시스템");
+	Func func;
+	
+	private void status() {
+		if(func.status.get(func.portName) == false) {
+			ON.setEnabled(false);
+			OFF.setEnabled(false);
+			Disconnect.setEnabled(false);
+			Connect.setEnabled(true);
+		}else if(func.status.get(func.portName) == true) {
+			ON.setEnabled(true);
+			OFF.setEnabled(true);
+			Connect.setEnabled(false);
+			Disconnect.setEnabled(true);
+		}
+	}
+	
 	public Test() {
 		
 		pa.setLayout(null);
-		ON.setBounds(20,60,70,50);
-		OFF.setBounds(110,60,70,50);
+		title.setBounds(20,10,260,30);
+		ON.setBounds(20,90,120,30);
+		OFF.setBounds(150,90,120,30);
+		Connect.setBounds(110,50,70,30);
+		Disconnect.setBounds(200, 50, 70, 30);
+
+		func = new Func("COM1");
+
+		status();
+		JComboBox<Object> select = new JComboBox<Object>(func.portNames);
+		select.setBounds(20,50,70,30);
 		ActionListener Onlisten = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -26,21 +62,51 @@ class Test extends JFrame{
 			public void actionPerformed(ActionEvent e) {
 				func.Off();
 			}
+		}; 
+		ActionListener Connectlisten = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				func.Connect();
+				status();
+			}
+		}; 
+		ActionListener Disconnectlisten = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				func.Disconnect();
+				status();
+			}
+		}; 
+		ActionListener Selectlisten = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JComboBox temp = (JComboBox<?>)e.getSource();
+				func.setPort(temp.getSelectedItem().toString());
+				status();
+			}
 		};
 		
+		title.setFont(new Font("맑은 고딕",Font.PLAIN,25));
+		
+		select.addActionListener(Selectlisten);
 		ON.addActionListener(Onlisten);
 		OFF.addActionListener(Offlisten);
-		pa.add(ON);
+		Connect.addActionListener(Connectlisten);
+		Disconnect.addActionListener(Disconnectlisten);
 		
+		pa.add(select);
+		pa.add(ON);
+		pa.add(Connect);
+		pa.add(Disconnect);
 		pa.add(OFF);
+		pa.add(title);
+		
 		add(pa);
 		
-		setSize(220,170);
+		setSize(300,170);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("에어컨 제어 시스템");
 		setVisible(true);
 	}
-
 }
 
 public class Main{
